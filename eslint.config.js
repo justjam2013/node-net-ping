97916771360b7cfce851943477378812fb4d4fd5
Just Jam <renzo.zanelli6@gmail.com>
2024-12-26 19:39:47 +0000

    Disable linting

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,10 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist/**'],
+    ignores: [
+      'dist/**',
+      'example./**'
+    ],
   },
   {
     rules: {
